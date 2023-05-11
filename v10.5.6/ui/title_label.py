2900f7f5dec449636345d749c97a5e33f5d06b7d
arisuvade <ariesdave253@gmail.com>
2023-05-11 22:45:45 +0800

    Add and fix title label

--- v10.5.6/ui/title_label.py
+++ v10.5.6/ui/title_label.py
@@ -5,15 +5,22 @@ class TitleLabel(ctk.CTkLabel):
     def __init__(self, parent, **kwargs):
         super().__init__(
             parent,
-            text="Type your message and how many.",
+            text="Pick a message type in options.",
             font=("Jetbrains Mono", 14, "bold"),
             **kwargs,
         )
 
         # Update title every 5 seconds
-        self.after(5000, self.update_title)
+        self.after(5000, self.update_title_1)
 
-    def update_title(self):
+    def update_title_1(self):
+        # Change title
+        self.configure(text="Enter a message and how many.")
+
+        # Call the method again after 5 seconds
+        self.master.after(5000, self.update_title_2)
+
+    def update_title_2(self):
         # Change title
         self.configure(text="Hover the mouse to end.")
 
@@ -22,7 +29,7 @@ class TitleLabel(ctk.CTkLabel):
 
     def reset_title(self):
         # Change title back to the original text
-        self.configure(text="Type your message and how many.")
+        self.configure(text="Pick a message type in options.")
 
         # Call the method again after 5 seconds
-        self.master.after(5000, self.update_title)
+        self.master.after(5000, self.update_title_1)
